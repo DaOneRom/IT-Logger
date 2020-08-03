147667d82ed8fb3528f8b18a5c88d3cd5e157faf
DaOneRom <davepmoreno@gmail.com>
2020-08-03 14:07:12 +0800

    Redux Store & Provider Setup

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -2,9 +2,10 @@ const initialState = {
 	logs: null,
 	current: null,
 	loading: false,
+	error: null,
 };
 
-export default (state, action) => {
+export default (state = initialState, action) => {
 	switch (action.type) {
 		default:
 			return state;
