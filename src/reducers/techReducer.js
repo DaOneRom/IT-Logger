beb9e521286dac0a844bdf7813ce3651a63262bf
DaOneRom <davepmoreno@gmail.com>
2020-08-19 18:44:00 +0800

    Techs Reducer, Action & Component

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,36 @@
+import {
+	GET_LOGS,
+	SET_LOADING,
+	LOGS_ERROR,
+	ADD_LOG,
+	DELETE_LOG,
+	UPDATE_LOG,
+	SEARCH_LOGS,
+	SET_CURRENT,
+	CLEAR_CURRENT,
+	GET_TECHS,
+} from '../actions/types';
+
+const initialState = {
+	techs: null,
+	loading: false,
+	error: null,
+};
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case GET_TECHS:
+			return {
+				...state,
+				techs: action.payload,
+				loading: false,
+			};
+		case SET_LOADING:
+			return {
+				...state,
+				loading: true,
+			};
+		default:
+			return state;
+	}
+};
